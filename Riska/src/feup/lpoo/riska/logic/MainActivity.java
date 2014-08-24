b0b17bd088b3ff7c0de12ad1ffdde5ded6af5d13
ludwig801 <ei12054@fe.up.pt>
2014-08-24 21:28:11 +0100

    MainActivity cleaned up

--- Riska/src/feup/lpoo/riska/logic/MainActivity.java
+++ Riska/src/feup/lpoo/riska/logic/MainActivity.java
@@ -8,7 +8,6 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
-import org.andengine.opengl.font.Font;
 import org.andengine.ui.activity.BaseGameActivity;
 
 import android.view.KeyEvent;
@@ -17,79 +16,67 @@ import feup.lpoo.riska.io.IOManager;
 import feup.lpoo.riska.resources.ResourceCache;
 
 public class MainActivity extends BaseGameActivity {	
-	
+
 	// ======================================================
 	// CONSTANTS
 	// ======================================================
-	public final static int CAMERA_WIDTH = 800;
-	public final static int CAMERA_HEIGHT = 500;
+	private final static int CAMERA_WIDTH = 800;
+	private final static int CAMERA_HEIGHT = 500;
 	private final float MAX_VELOCITY = 700f;
-	
-	public final float FRAME_RATE = 1 / 60f;
-	public final float LIMIT_FRAME_RATE = 1 / 30f;
 
-	
-	public final static float RES_RATIO = 8f/5f;
-	
-	public Font mFont;
+	public final static float RES_RATIO = 16f/10f;
+
+	// ======================================================
+	// FIELDS
+	// ======================================================
 	public CameraManager mCamera;
-	
-	public Scene mCurrentScene;
 	public static MainActivity instance;
-	
-	protected SceneManager sceneManager;
-	protected ResourceCache resources;
 
+	// ======================================================
+	// ======================================================
 	@Override
-	public EngineOptions onCreateEngineOptions() {
-		
+	public EngineOptions onCreateEngineOptions()
+	{
 		instance = this;
-		
+
 		mCamera = new CameraManager(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT, MAX_VELOCITY, MAX_VELOCITY, 3.0f);
 
 		mCamera.setBounds(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		mCamera.setBoundsEnabled(true);
+		
 		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR,
 				new RatioResolutionPolicy(RES_RATIO), mCamera);
-		
-		//Log.d("Riska", "Camera Size: " + mCamera.getWidth() + ", " + mCamera.getHeight());
-		
+
 		engineOptions.getAudioOptions().setNeedsMusic(true);
 		return engineOptions;
 	}
 
 	@Override
-	public void onCreateResources(
-			OnCreateResourcesCallback pOnCreateResourcesCallback)
-			throws IOException {
-		
+	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException
+	{
 		ResourceCache.prepareManager(mEngine, this, mCamera, getVertexBufferObjectManager());	
 		ResourceCache.getSharedInstance().loadSplashScene();
 		ResourceCache.getSharedInstance().createMainMenuResources();
 		ResourceCache.getSharedInstance().createGameResources();
-		
+
 		IOManager.loadGameOptions();
-		
+
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
-		
 	}
 
 	@Override
-	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
-			throws IOException
-			{
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException
+	{
 		SceneManager.getSharedInstance().createSplashScene(pOnCreateSceneCallback);
 	}
 
 	@Override
-	public void onPopulateScene(Scene pScene,
-			OnPopulateSceneCallback pOnPopulateSceneCallback)
-			throws IOException {
-		
+	public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException
+	{
 		float seconds = 2.0f;
-		
-		mEngine.registerUpdateHandler(new TimerHandler(seconds, new ITimerCallback() {
 
+		mEngine.registerUpdateHandler(new TimerHandler(seconds, new ITimerCallback()
+		{
 			@Override
 			public void onTimePassed(TimerHandler pTimerHandler)
 			{
@@ -97,22 +84,21 @@ public class MainActivity extends BaseGameActivity {
 				SceneManager.getSharedInstance().createMainMenuScene();
 			}
 		}));
-		
-		pOnPopulateSceneCallback.onPopulateSceneFinished();
-		
+
+		pOnPopulateSceneCallback.onPopulateSceneFinished();	
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event)
 	{
-		if(keyCode == KeyEvent.KEYCODE_BACK) {
+		if(keyCode == KeyEvent.KEYCODE_BACK)
+		{
 			SceneManager.getSharedInstance().getCurrentScene().onBackKeyPressed();
 		}
-		return false;
+		
+		return super.onKeyDown(keyCode, event);
 	}
 
-	public static MainActivity getSharedInstance() {
-		 return instance;
-	}
-	
+	public static MainActivity getSharedInstance() { return instance; }
+
 }
